f55d80011adefbed061e2c531349a8c27923aa3f
PaigeeQ <Pa.krajcova@gmail.com>
2025-03-26 20:33:55 +0100

    pridan inndex js dle zadani

--- index.js
+++ index.js
@@ -15,3 +15,30 @@ const product3 = {
   name: 'Anatoplavky',
   description: 'Aby muži na rozpálených plážích kromě vašeho těla obdivovali také vaše nitro.',
 };
+
+const product1Element = document.querySelector("#product1"); 
+product1Element.classList.add("border-primary");
+
+const product2ButtonElement = document.querySelector("#product2 button"); 
+product2ButtonElement.classList.remove("btn-primary");
+
+const product3TitleElement = document.querySelector("#product3 .card-title");
+product3TitleElement.classList.toggle("text-center");
+
+/*Vytvořte si repozitář ze šablony cviceni-nekupto. Repozitář obsahuje stránku, která 
+nabízí několik neotřelých produktů.
+
+Otevřete si naklonovanou složku ve VS Code a prohlédněte si zdrojové HTML. Stránka 
+je nastylována pomocí CSS knihovny Bootstrapu. Co přesně dělají použité CSS třídy 
+není pro toto cvičení podstatné, nemusíte jim věnovat velkou pozornost.
+V souboru index.js si na posledním řádku do proměnné uložte kartu s prvním produktem. 
+Pomocí metody classList.add přidejte na tento element třídu border-primary, abychom 
+první produkt na stránce zvýraznili.
+Do jiné proměnné si uložte tlačítko na druhé kartě. Pomocí metody classList.remove 
+odeberte třídu btn-primary a podívejte se, jak se tlačítko vizuálně změnilo.
+Do další proměnné si uložte element s třídou card-title posledního produktu. Pomocí 
+voláni metody classList.toggle přidejte na tento element třídu text-center. Text by se 
+měl tímto zarovnat na střed. Vyzkoušejte si, že když tuto metodu zavoláte znova, třída 
+se z prvku odstraní. Takto můžete přepínat mezi přidáním a odebráním třídy pomocí opakovaného 
+volání této metody.
+*/
